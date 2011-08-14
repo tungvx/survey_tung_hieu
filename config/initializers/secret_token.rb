b6997534230b687f146b382396626a13643430d9
Vu Xuan Tung <toilatung90@gmail.com>
2011-08-15 01:07:31 +0700

    init project with devise gem and user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survey::Application.config.secret_token = '98ef11ac20396144bb96f7253e552ef08b74d8702ee0fcc9e996ee47400b51e7670f66d2f9c7630e83c31a711d0dc2df8696d368c13f26ad5140a08187adcb8f'
