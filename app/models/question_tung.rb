181531f3015b2fd79dff9ac4938efee31a610d3a
Vu Xuan Tung <toilatung90@gmail.com>
2011-08-15 10:07:07 +0700

    add associations

--- app/models/question_tung.rb
+++ app/models/question_tung.rb
@@ -1,2 +1,4 @@
 class QuestionTung < ActiveRecord::Base
+   belongs_to :survey_tung
+   has_one :answer
 end
--- app/models/survey_tung.rb
+++ app/models/survey_tung.rb
@@ -1,2 +1,4 @@
 class SurveyTung < ActiveRecord::Base
+   belongs_to :user
+   has_many :question_tungs
 end
