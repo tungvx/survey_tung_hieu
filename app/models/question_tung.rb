c40f456ed37bc4eada571541f5d54c49a9f566a1
Vu Xuan Tung <toilatung90@gmail.com>
2011-08-29 15:29:06 +0700

    add validate

--- app/models/question_tung.rb
+++ app/models/question_tung.rb
@@ -2,4 +2,5 @@ class QuestionTung < ActiveRecord::Base
    belongs_to :survey_tung
    has_many :answers, :dependent => :destroy
    accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+  validates_presence_of :content, :question_type
 end
--- app/models/survey_tung.rb
+++ app/models/survey_tung.rb
@@ -2,4 +2,5 @@ class SurveyTung < ActiveRecord::Base
    belongs_to :user
    has_many :question_tungs, :dependent => :destroy
    accepts_nested_attributes_for :question_tungs, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+   validates_presence_of :title
 end
